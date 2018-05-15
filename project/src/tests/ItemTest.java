2112b3f36acff5b43adbbabeded9877c5dfe41a0
Tim Melville <timothy.melville@connect.qut.edu.au>
2018-05-15 20:38:11 +1000

    Item and Stock Tests

--- project/src/tests/ItemTest.java
+++ project/src/tests/ItemTest.java
@@ -2,7 +2,142 @@ package tests;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import objects.Item;
+import exceptions.*;
 
 public class ItemTest {
-
+	
+	private Item apple;
+	
+	/**
+	 * Construction of item with no temperature control
+	 */
+	@Test
+	public void constructItem() {
+		apple = new Item("apple", 1, 2, 3, 4);
+	}
+	
+	/**
+	 * Construction of item with temperature control
+	 */
+	@Test
+	public void constructControlledItem() {
+		apple = new Item("apple", 1, 2, 3, 4, 5);
+	}
+	
+	/**
+	 * Getting purchase cost of Item
+	 */
+	@Test
+	public void getCost() {
+		assertEquals(1, apple.getCost());
+	}
+	
+	/**
+	 * Can't construct Item with zero cost
+	 */
+	@Test (expected = StockException.class)
+	public void ZeroCost() throws StockException {
+		apple = new Item("apple", 0, 2, 3, 4);
+	}
+	
+	/**
+	 * Can't construct Item with negative cost
+	 */
+	@Test (expected = StockException.class)
+	public void NegativeCost() throws StockException {
+		apple = new Item("apple", -1, 2, 3, 4);
+	}
+	
+	/**
+	 * Can't construct Item with zero sell price
+	 */
+	@Test (expected = StockException.class)
+	public void ZeroSellPrice() throws StockException {
+		apple = new Item("apple", 1, 0, 3, 4);
+	}
+	
+	/**
+	 * Can't construct Item with negative sell price
+	 */
+	@Test (expected = StockException.class)
+	public void NegativeSellPrice() throws StockException {
+		apple = new Item("apple", 1, -2, 3, 4);
+	}
+	
+	/**
+	 * Can't construct Item with negative re-order point
+	 */
+	@Test (expected = StockException.class)
+	public void NegativeReorderPoint() throws StockException {
+		apple = new Item("apple", 1, 2, -3, 4);
+	}
+	
+	/**
+	 * Can't construct Item with zero re-order amount
+	 */
+	@Test (expected = StockException.class)
+	public void ZeroReorderAmount() throws StockException {
+		apple = new Item("apple", 1, 2, 3, 0);
+	}
+	
+	/**
+	 * Can't construct Item with negative re-order amount
+	 */
+	@Test (expected = StockException.class)
+	public void NegativeReorderAmount() throws StockException {
+		apple = new Item("apple", 1, 2, 3, -4);
+	}
+	
+	/**
+	 * Getting sell price
+	 */
+	@Test
+	public void getPrice() {
+		apple = new Item("apple", 1, 2, 3, 4);
+		assertEquals(2, apple.getSellPrice());
+	}
+	
+	/**
+	 * Getting re-order point
+	 */
+	@Test
+	public void getReorderPoint() {
+		assertEquals(3, apple.getReorderPoint());
+	}
+	
+	/**
+	 * Getting re-order amount
+	 */
+	@Test
+	public void getReorderAmount() {
+		assertEquals(4, apple.getReorderAmount());
+	}
+	
+	/**
+	 * Checking whether Item is temperature controlled
+	 */
+	@Test
+	public void testTemperature() {
+		assertFalse(apple.isTempControlled());
+	}
+	
+	/**
+	 * Getting maximum temperature
+	 */
+	@Test
+	public void getTemp() {
+		apple = new Item("apple", 1, 2, 3, 4, 5);
+		assertEquals(5, apple.getTemp());
+	}
+	
+	/**
+	 * Can't get maximum temperature of a dry item
+	 */
+	@Test (expected = stockException.class)
+	public void noTemp() throws StockException {
+		apple = new Item("apple", 1, 2, 3, 4);
+		assertEquals(11, apple.getTemp());
+	}
+	
 }
--- project/src/tests/StockTest.java
+++ project/src/tests/StockTest.java
@@ -2,7 +2,170 @@ package tests;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import objects.*;
+import exceptions.*;
 
 public class StockTest {
 
+	private Item apple;
+	private Item banana;
+	private Stock fruits;
+	
+
+	/**
+	 * Creates Stock object
+	 * Creates two Items to use in tests
+	 * Stock class depends on Item class, so Item class must be complete
+	 */
+	@Test
+	public void createStock() {
+		fruits = new Stock();
+		apple = new Item("apple", 1, 1, 1, 1, -10);
+		banana = new Item("banana", 1, 1, 1, 1, 10);
+	}
+	
+	/**
+	 * Modifying quantity of Item not in Stock adds Item to Stock
+	 */
+	@Test
+	public void addNewItem() {
+		fruits.modifyQuantity(apple, 5);
+		assertEquals(5, fruits.getQuantity(apple));
+	}
+	
+	/**
+	 * Check whether Item exists in Stock
+	 */
+	@Test
+	public void itemPresent() {
+		assertTrue(fruits.itemInStock(apple));
+	}
+	
+	/**
+	 * Check whether Item does not exist in Stock
+	 */
+	@Test
+	public void itemNotPresent() {
+		assertFalse(fruits.itemInStock(banana));
+	}
+	
+	/**
+	 * Adds additional quantity to Item already in Stock
+	 */
+	@Test
+	public void addToExistingItem() {
+		fruits.modifyQuantity(apple, 5);
+		assertEquals(10, fruits.getQuantity(apple));
+	}
+	
+	/**
+	 * Removes quantity from Item already in Stock
+	 */
+	@Test 
+	public void removeFromExistingItem() {
+		fruits.modifyQuantity(apple, -5);
+		assertEquals(5, fruits.getQuantity(apple));
+	}
+	
+	/**
+	 * Cannot remove quantity from Item not in Stock
+	 * @throws StockException
+	 */
+	@Test (expected = StockException.class)
+	public void removeFromNonExistingItem() throws StockException {
+		fruits.modifyQuantity(banana, -5);
+	}
+	
+	/**
+	 * Cannot reduce an Item's quantity past zero
+	 * @throws StockException
+	 */
+	@Test (expected = StockException.class)
+	public void quantityToNegative() throws StockException {
+		//Attempts to bring apple quantity from 5 to -5
+		fruits.modifyQuantity(apple, -10);
+	}
+	
+	/**
+	 * Gets quantity of all Items in Stock
+	 */
+	@Test
+	public void getTotalQuantity() {
+		// Adds banana to fruits with a quantity of 1
+		fruits.modifyQuantity(banana, 1);
+		// Apple quantity should previously be 5
+		assertEquals(6, fruits.getTotalQuantity());
+	}
+	
+	/**
+	 * When no Items are in Stock, quantity should still exist
+	 */
+	@Test
+	public void getEmptyQuantity() {
+		fruits = new Stock();
+		assertEquals(0, fruits.getTotalQuantity());
+	}
+	
+	/**
+	 * Checks whether any Items in Stock are temperature controlled
+	 */
+	@Test
+	public void hasTempRequirement() {
+		apple = new Item("apple", 1, 2, 3, 4, 5);
+		fruits.modifyQuantity(apple, 10);
+		assertTrue(fruits.needsTempControl());
+	}
+	
+	/**
+	 * Gets minimum required temperature of all Items in Stock
+	 */
+	@Test
+	public void getMinTemp() {
+		apple = new Item("apple", 1, 1, 1, 1, -10);
+		banana = new Item("banana", 1, 1, 1, 1, 5);
+		fruits = new Stock();
+		fruits.modifyQuantity(apple, 1);
+		fruits.modifyQuantity(banana, 1);
+		
+		assertEquals(-10, fruits.getMinTemp());
+	}
+	
+	/**
+	 * Cannot get minimum required temperature of purely non-controlled Items
+	 */
+	@Test (expected = StockException.class)
+	public void getTempNotControlled() throws StockException(){
+		apple = new Item("apple", 1, 1, 1, 1);
+		banana = new Item("banana", 1, 1, 1, 1);
+		fruits = new Stock();
+		fruits.modifyQuantity(apple, 1);
+		fruits.modifyQuantity(banana, 1);
+		
+		fruits.getMinTemp();
+	}
+	
+	/**
+	 * Removes desired number of Items from one Stock and places in another
+	 * Lowest temperature Items removed first
+	 */
+	@Test
+	public void takeSomeStock() {
+		Stock fruitBasket;
+		apple = new Item("apple", 1, 2, 3, 4, 5);
+		banana = new Item("banana", 6, 7, 8, 9, 10);
+		fruits = new Stock();
+		
+		fruits.modifyQuantity(apple, 5);
+		fruits.modifyQuantity(banana, 5);
+		
+		fruitBasket = fruits.take(6);
+		
+		//fruitbasket should contain the 6 coldest Items from fruits
+		//That is, 5 apples and 1 banana
+		//fruits should contain 4 bananas
+		assertEquals(6, fruitBasket.getTotalQuantity());
+		assertEquals(4, fruits.getTotalQuantity());
+		assertEquals(10, fruits.getMinTemp());
+	}
+	
 }
