f28df83df585627b3284474ef4330fdc04eba21f
Dean McHugh <n9656286@qut.edu.au>
2018-05-18 18:36:47 +1000

    truck testing class

--- project/src/tests/TruckTest.java
+++ project/src/tests/TruckTest.java
@@ -3,6 +3,39 @@ package tests;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import delivery.RefridgeratedTruck;
+
 public class TruckTest {
 
+	private Truck truck;
+	
+	@Test 
+	public void constructOrdinaryTruck() {
+		truck = new OrdinaryTruck(100, 100, "apple");
+	}
+	
+	@Test
+	public void constructRefrigeratedTruck() {
+		truck = new RefrigeratedTruck(100, 100, "apple", 10);
+	}
+	
+	@Test
+	public void testRefrigeratedGetCost() {
+		assertEquals(100, truck.getCost());
+	}
+	
+	@Test
+	public void testRefrigeratedGetCargoCap() {
+		assertEquals(100, truck.getCargoCap());
+	}
+	
+	@Test
+	public void testGetCargo() {
+		assertEquals("apple", truck.getCargo());
+	}
+	
+	@Test
+	public void testGetTemp() {
+		assertEquals(10, truck.getTemp());
+	}
 }
