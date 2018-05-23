59e2ca29834689b12915e8132075cd2585865213
Tim Melville <timothy.melville@connect.qut.edu.au>
2018-05-23 15:39:23 +1000

    Initial implementation of manifest class based on usage in csv classes

--- project/src/delivery/Manifest.java
+++ project/src/delivery/Manifest.java
@@ -1,5 +1,44 @@
 package delivery;
 
-public class Manifest {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+/**
+ * Stores a collection of Truck objects in an iterable format.
+ * @author Tim
+ */
+public class Manifest implements Iterable<Truck> {
+
+	private ArrayList<Truck> truckList;
+	
+	
+	/**
+	 * Constructor for Manifest, initializes arraylist to store Trucks.
+	 * @author Tim
+	 */
+	public Manifest() {
+		truckList = new ArrayList<Truck>();
+	}
+	
+	
+	/**
+	 * Adds a Truck to the manifest.
+	 * @param truck Truck object to be added to the manifest.
+	 * @author Tim
+	 */
+	public void addTruck(Truck truck) {
+		truckList.add(truck);
+	}
+
+	
+	/**
+	 * Method to allow a for each loop for Trucks within the Manifest. Returns the iterator of the ArrayList storing Trucks.
+	 * @return Iterator for next Truck in Manifest.
+	 * @author Tim
+	 */
+	@Override
+	public Iterator<Truck> iterator() {
+		return truckList.iterator();
+	}
+	
 }
