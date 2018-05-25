2a47a3cb1c7ed405919b4982eb34a61236fbabe6
Tim Melville <timothy.melville@connect.qut.edu.au>
2018-05-25 10:36:25 +1000

    Additional constructor for Manifest to be used when creating store orders

--- project/src/delivery/Manifest.java
+++ project/src/delivery/Manifest.java
@@ -3,8 +3,8 @@ package delivery;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import objects.Item;
-import objects.Stock;
+import objects.*;
+import exceptions.*;
 
 /**
  * Stores a collection of Truck objects in an iterable format.
@@ -21,6 +21,32 @@ public class Manifest implements Iterable<Truck> {
 	 */
 	public Manifest() {
 		truckList = new ArrayList<Truck>();
+	}
+	
+	
+	/**
+	 * Additional constructor for Manifest. Takes a Stock object, and divides this up into Trucks.
+	 * @param totalOrder Stock object containing Items and quantity of Items ordered.
+	 * @throws DeliveryException
+	 * @throws StockException
+	 */
+	public Manifest(Stock totalOrder) throws DeliveryException, StockException {
+		truckList = new ArrayList<Truck>();
+		Truck newTruck;
+		int numItemsInTruck;
+		
+		//Keep adding trucks until order is completed
+		while (totalOrder.getTotalQuantity() > 0) {
+			if (totalOrder.needsTempControl()){
+				//Ensure truck is not over-filled
+				numItemsInTruck = Math.min(800, totalOrder.getTotalQuantity()); //TODO: Remove magic number
+				newTruck = new RefrigeratedTruck(totalOrder.take(numItemsInTruck));
+			} else {
+				numItemsInTruck = Math.min(1000, totalOrder.getTotalQuantity()); //TODO: Remove magic number
+				newTruck = new OrdinaryTruck(totalOrder.take(numItemsInTruck));
+			}
+			this.addTruck(newTruck);
+		}	
 	}
 	
 	
