83209d91aed0e9c9e016d121f95dcb0afce2e963
Tim Melville <timothy.melville@connect.qut.edu.au>
2018-05-25 10:42:30 +1000

    Added program logic structure outline in main.java

--- project/src/GUI/main.java
+++ project/src/GUI/main.java
@@ -1,11 +1,65 @@
 package GUI;
 
-import objects.Store;
+import csv.*;
+import delivery.*;
+import exceptions.*;
+import objects.*;
 
 public class main {
 	
 	public static void main(String[] args) {
 		new GUI();
+	}
+
+	//Not to be implemented as a straight method like this. Each chunk of code should be executed on a button press
+	public static void logicLoop() throws CSVFormatException, StockException, DeliveryException {
+		//GUI STARTS
+		Store shopFront = Store.getInstance();
+		shopFront.setName("Local Store");
+		
+		
+		//BUTTON PRESSED "READ ITEM PROPERTIES"
+		Stock itemList = FileRead.readProperties();
+		
+		//BUTTON PRESSED EXPORT MANIFEST
+		Manifest shippingOrder = new Manifest(itemList);
+		FileWrite.writeManifest(shippingOrder);
+		
+		
+		while(true) {
+			//BUTTON PRESSED IMPORT MANIFEST
+			//Read manifest, load shop with contents, reduce store capital, reset order list
+			shippingOrder = FileRead.readManifest();
+			for (Truck truck : shippingOrder) {
+				Stock contents = truck.getCargo();
+				for (Item item : contents) {
+					shopFront.addToInventory(item, contents.getQuantity(item));
+				}
+			}
+			shopFront.reduceCapital(shippingOrder.getTotalCost());
+			shopFront.resetOrderList();
+			
+			//BUTTON PRESSED IMPORT SALES LOG
+			//Load sales log, change store quantities, add necessary items to order list (Automatically handled by store class)
+			Stock sales;
+			try {
+				sales = FileRead.readSalesLog();
+			} catch (CSVFormatException fileException) {
+				//End program as next sales log does not seem to exist
+				return;
+			}
+			
+			for (Item itemSold : sales) {
+				shopFront.sellItem(itemSold, sales.getQuantity(itemSold));
+			}
+			
+			
+			//BUTTON PRESSED EXPORT MANIFEST
+			//Write new manifest from shop's order list
+			shippingOrder = new Manifest(shopFront.getOrderList());
+			FileWrite.writeManifest(shippingOrder);
+		}
+		
 	}
 
 }
